38cac8299fa16941e94f7acaf727dbae613f8ba0
James Grimmelmann <james.grimmelmann@cornell.edu>
2019-02-15 13:18:34 -0500

    CTECH 402 Module 4

---
+++ ctech402/module_4/courses1.py
@@ -0,0 +1,6 @@
+rooms = {'PHYS 2033': 'Sciences 131', 'CHEM 6512': 'Sciences 165', 'NBAY 5400': 'Annex L23', 'NBAY 5500': 'Sciences 131', 'BIOL 5230': 'Sciences B61', 'COMP 5100': 'Annex 141', 'PHYS 2132': 'Sciences 161', 'PHYS 2140': 'Sciences 161', 'PHYS 2385': 'Sciences 131', 'MATH 5411': 'Sciences 165', 'INFO 5310': 'Sciences 161', 'INFO 5340': 'Sciences B61'}
+
+schedule = ['PHYS 2033', 'BIOL 5230', 'INFO 5310', 'MATH 5411', 'COMP 5100']
+
+for course in schedule:
+    print (course + ' meets in ' + rooms[course])
---
+++ ctech402/module_4/courses2.py
@@ -0,0 +1,4 @@
+rooms = {'PHYS 2033': 'Sciences 131', 'CHEM 6512': 'Sciences 165', 'NBAY 5400': 'Annex L23', 'NBAY 5500': 'Sciences 131', 'BIOL 5230': 'Sciences B61', 'COMP 5100': 'Annex 141', 'PHYS 2132': 'Sciences 161', 'PHYS 2140': 'Sciences 161', 'PHYS 2385': 'Sciences 131', 'MATH 5411': 'Sciences 165', 'INFO 5310': 'Sciences 161', 'INFO 5340': 'Sciences B61'}
+
+for course in rooms:
+    print (course + ' meets in ' + rooms[course])
