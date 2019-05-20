0a3ba552034cc5b8c0b11137b6265a5aeab9671d
James Grimmelmann <james.grimmelmann@cornell.edu>
2019-05-20 16:15:04 -0400

    CTECH403 module 6 edits

--- ctech403/module_6/peewee-sqlite.py
+++ ctech403/module_6/peewee-sqlite.py
@@ -1,16 +1,41 @@
 from peewee import *
 
-db = SqliteDatabase('menu.sqlite')
+db = SqliteDatabase('registrar-peewee.sqlite')
 
-class Items(Model):
-    Name = CharField()
-    Price = CharField()
-    Type = CharField()
+class BaseModel(Model):
     class Meta:
         database = db
 
-for i in Items.select():
-    print (i.Name + ': ' + i.Price)
+class Students(BaseModel):
+    lastname = TextField()
+    firstname = TextField()
+    studentid = IntegerField(primary_key=True)
 
-mainsList = Items.select().where(Items.Type == 'Mains')
-print ('There are ' + str(len(mainsList)) + ' main dishes.')
+class Courses(BaseModel):
+    courseid = IntegerField(primary_key=True)
+    coursename = TextField()
+    roomid = IntegerField()
+    times = TextField()
+    credits = IntegerField()
+
+class Rooms(BaseModel):
+    roomid = IntegerField(primary_key=True)
+    roomname = TextField()
+    capacity = IntegerField()
+    
+class Enrollments(BaseModel):
+    studentid = IntegerField()
+    courseid = IntegerField()
+    class Meta:
+        primary_key = False
+
+for i in Students.select():
+    print (i.firstname + ' ' + i.lastname)
+print()
+
+for i in Rooms.select().where(Rooms.capacity > 20):
+    print (i.roomname + ' has capacity ' + str(i.capacity))
+print()
+
+for i in Courses.select().join(Rooms, on = (Courses.roomid == Rooms.roomid)).where(Rooms.capacity > 20):
+    print(i.coursename + ' meets in a room with capacity > 20')
--- ctech403/module_6/sqlite.py
+++ ctech403/module_6/sqlite.py
@@ -1,14 +1,17 @@
 import sqlite3
 
-dbname = 'menu.sqlite'
+dbname = 'registrar.sqlite'
 
 connection = sqlite3.connect(dbname)
 c = connection.cursor()
 
-c.execute('SELECT * FROM items;')
+c.execute('SELECT * FROM courses;')
 rows = c.fetchall()
+print("\nAll fields from all courses")
 print(rows)
 
-c.execute('SELECT * FROM items WHERE Type = \'Mains\';')
+
+c.execute('SELECT coursename FROM courses WHERE times = \'MW10\';')
 rows = c.fetchall()
+print("\nCourse names meeting MW at 10")
 print(rows)
