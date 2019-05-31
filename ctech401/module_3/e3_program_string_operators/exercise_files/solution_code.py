d783c3211ad617c56e600cb6d1ffc2489b8e3698
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-05-31 14:46:18 -0400

    Update solution_code.py

--- ctech401/module_3/e3_program_string_operators/exercise_files/solution_code.py
+++ ctech401/module_3/e3_program_string_operators/exercise_files/solution_code.py
@@ -1,4 +1,4 @@
 city = input("What is your city?")
 state= input("What is your state?")
-my_home = "I live in " + city + ", " + state
-print(my_home)
+my_address = "I live in " + city + ", " + state
+print(my_address)
