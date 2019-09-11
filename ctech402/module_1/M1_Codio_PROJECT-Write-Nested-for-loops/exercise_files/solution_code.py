e6edbd5f495bdea4344531b0f409fe018f33fd40
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-11 18:45:30 -0400

    Create solution_code.py

---
+++ ctech402/module_1/M1_Codio_PROJECT-Write-Nested-for-loops/exercise_files/solution_code.py
@@ -0,0 +1,6 @@
+dish = ["pasta alfredo", "baked salmon", "chicken parmesan", "sliders"]
+side = ["french fries", "mashed potatoes", "salad"]
+
+for i in dish:
+	for j in side:
+		print("You should order " + i + " and " + j)
