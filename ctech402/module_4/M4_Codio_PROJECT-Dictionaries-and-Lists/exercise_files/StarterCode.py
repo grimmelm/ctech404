46e8d1cc6e21e2170874bac8d7e1b052a4f829e7
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-17 16:45:44 -0400

    Update StarterCode.py

--- ctech402/module_4/M4_Codio_PROJECT-Dictionaries-and-Lists/exercise_files/StarterCode.py
+++ ctech402/module_4/M4_Codio_PROJECT-Dictionaries-and-Lists/exercise_files/StarterCode.py
@@ -1,1 +1,2 @@
 office_numbers = {'Frank Pietro': 204, 'Jennifer Moth': 103, 'Barbara Wilson': 245, 'Mark Opengher' : 215, 'Ronald Davis': 335, 'Minnie Potter': 207, 'Joseph Smith': 130, 'Christopher Neals': 167, 'Georgia Thompson': 307}
+office_updates = {215: 370, 207: 304, 245: 325, 204: 399} 
