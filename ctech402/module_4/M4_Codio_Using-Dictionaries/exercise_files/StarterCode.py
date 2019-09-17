ed8384136163df28f4d83681e5d41995020e29d5
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-17 14:14:10 -0400

    Update StarterCode.py

--- ctech402/module_4/M4_Codio_Using-Dictionaries/exercise_files/StarterCode.py
+++ ctech402/module_4/M4_Codio_Using-Dictionaries/exercise_files/StarterCode.py
@@ -1,1 +1,1 @@
-weekly_planner = { 'Monday': 'hair appointment' , 'Tuesday': 'pick up groceries', 'Wednesday': 'baseball practice', Thursday: 'wash car', Friday: 'dinner with friends'}
+weekly_planner = { 'Monday': 'hair appointment' , 'Tuesday': 'pick up groceries', 'Wednesday': 'baseball practice', 'Thursday': 'wash car', 'Friday': 'dinner with friends'}
