6e7f94f7849bdfe68b95d864763d69941e52f84d
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-18 17:04:02 -0400

    Update StarterCode.py

--- ctech402/module_5/M5_Codio_Modules/StarterCode.py
+++ ctech402/module_5/M5_Codio_Modules/StarterCode.py
@@ -2,10 +2,15 @@ import stats
 
 my_list = [4,1,5,7,6,8,9,10,8,3,3,8,12]
 
-print('The mean is : ' +  str(stats.mean(my_list))
 
-print('The median is : ' +  str(stats.median(my_list))
+mean  = stats.mean(my_list)
+print('The mean is: ' +  str(mean))
 
-print('The range is : ' +  str(stats.range(my_list))
+median = stats.median(my_list)
+print('The median is: ' +  str(median))
 
-print('The mode is : ' + str(stats.mode(my_list))
+range = stats.range(my_list)
+print('The range is: ' +  str(range))
+
+sum = stats.range(my_list)
+print('The sum of all numbers is: ' + str(sum))
