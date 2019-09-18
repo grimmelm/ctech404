1232d5e80b4a744a6c9c9e72c79a55be26cb0acb
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-18 15:19:42 -0400

    Update SolutionCode1.py

--- ctech402/module_5/M5_Codio_Defining-Functions/exercise_files/SolutionCode1.py
+++ ctech402/module_5/M5_Codio_Defining-Functions/exercise_files/SolutionCode1.py
@@ -6,4 +6,7 @@ def absolute_value(number):
   return number
   
   
-print(absolute_value())
+print(absolute_value(-5))
+print(absolute_value(-12.7))
+print(absolute_value(45))
+print(absolute_value(5*2-17))
