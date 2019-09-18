b91dfbb102f3404f05446e6abfe3e60e780af952
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-18 16:55:57 -0400

    Create StarterCode.py

---
+++ ctech402/module_5/M5_Codio_Modules/StarterCode.py
@@ -0,0 +1,11 @@
+import stats
+
+my_list = [4,1,5,7,6,8,9,10,8,3,3,8,12]
+
+print("The mean is : " str(stats.mean(my_list))
+
+print("The median is : " str(stats.median(my_list))
+
+print("The range is : " str(stats.range(my_list))
+
+print("The mode is : " str(stats.mode(my_list))
