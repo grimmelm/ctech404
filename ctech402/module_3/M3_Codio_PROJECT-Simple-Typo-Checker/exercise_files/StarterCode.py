bdf83629b960185f07877b593c05236da105c16b
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-19 13:49:27 -0400

    Update StarterCode.py

--- ctech402/module_3/M3_Codio_PROJECT-Simple-Typo-Checker/exercise_files/StarterCode.py
+++ ctech402/module_3/M3_Codio_PROJECT-Simple-Typo-Checker/exercise_files/StarterCode.py
@@ -3,5 +3,8 @@ tiny little bed. By now she was feeling rather tired. so she climbed into the bi
 Then she tried the middle-sized bed, but that was far too soft. so she climbed into the tiny little bed. 
 It was neither too hard nor too soft. In fact, it felt just right, all cosy and warm. and in no tine at all Goldilocks fell fast asleep.'
 
-spelling_errors = []
-corrections = []
+spelling_errors = ['wear' , 'tre', 'bord', 'burd', 'biig', 'two', 'write', 'crazy', 'tine']
+corrections = ['where' , 'three', 'bed', 'bed', 'big', 'too', 'right', 'cozy', 'time']
+
+
+
