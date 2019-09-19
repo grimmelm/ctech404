93164e93cbdce67ada1c6dcc3e66c21a30cb020e
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-19 13:41:46 -0400

    Create StarterCode.py

---
+++ ctech402/module_3/M3_Codio_PROJECT-Simple-Typo-Checker/exercise_files/StarterCode.py
@@ -0,0 +1,7 @@
+fairy_tale = 'Next Goldilocks went upstairs, where she found three beds. There was a great big bed, a middle-sized bed and a 
+tiny little bed. By now she was feeling rather tired. so she climbed into the big bed and lay down. The big bed was very hard and far too big. 
+Then she tried the middle-sized bed, but that was far too soft. so she climbed into the tiny little bed. 
+It was neither too hard nor too soft. In fact, it felt just right, all cosy and warm. and in no tine at all Goldilocks fell fast asleep.'
+
+spelling_errors = []
+corrections = []
