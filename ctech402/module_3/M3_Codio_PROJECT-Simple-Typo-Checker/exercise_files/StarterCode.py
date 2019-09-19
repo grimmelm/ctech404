16ab148ff789c380fe821a96c9a08028cab5e0b4
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-19 14:12:17 -0400

    Update StarterCode.py

--- ctech402/module_3/M3_Codio_PROJECT-Simple-Typo-Checker/exercise_files/StarterCode.py
+++ ctech402/module_3/M3_Codio_PROJECT-Simple-Typo-Checker/exercise_files/StarterCode.py
@@ -1,6 +1,7 @@
-fairy_tale = 'Next Goldilocks went upstairs, wear she found tre beds. There was a great big bord, a middle-sized bord and a tiny little bord. By now she was feeling rather tired. so she climbed into the big burd and lay down. The big bord was very hard and far too biig. Then she tried the middle-sized bord, but that was far too soft. so she climbed into the tiny little burd. It was neither two hard nor too soft. In fact, it felt just write, all crazy and warm and in no tine at all Goldilocks fell fast asleep.'
+fairy_tale = 'Next Goldilocks went upstairs, wear she found tre beds. There was a great big bord, a middle-sized bed and a tiny little bard. By now she was feeling rather tired. so she climbed into the big beed and lay down. The big bord was very hard and far too biig. Then she tried the middle-sized bud, but that was far too soft. so she climbed into the tiny little bed. It was neither two hard nor too soft. In fact, it felt just write, all crazy and warm and in no tine at all Goldilocks fell fast asleep.'
 
-spelling_errors = ['wear' , 'tre', 'bord', 'burd', 'biig', 'two', 'write', 'crazy', 'tine']
-corrections = ['where' , 'three', 'bed', 'bed', 'big', 'too', 'right', 'cozy', 'time']
+
+spelling_errors = ['wear' , 'tre', 'bord', 'bard', 'beed', 'biig', 'two', 'write', 'crazy', 'tine']
+corrections =     ['where' , 'three', 'bed', 'bed', 'bed', 'big', 'too', 'right', 'cozy', 'time']
 
 
