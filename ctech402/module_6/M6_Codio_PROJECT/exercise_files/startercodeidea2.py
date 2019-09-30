b25d4c08110f2dae1780cda973ea59fa3f5d2af0
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-09-30 14:12:17 -0400

    Add files via upload

---
+++ ctech402/module_6/M6_Codio_PROJECT/exercise_files/startercodeidea2.py
@@ -0,0 +1,31 @@
+####
+# 
+# Player and Comp each have 5 dice
+# They compete for three rounds:
+# 	They each roll all 5 dice 3 times 
+# User is allowed to re-roll all 5 dice once during each round
+#
+#######
+
+import random
+
+# total score
+player_score = 0
+computer_score = 0
+
+# save the score for each round
+player_hand = []
+computer_hand  = []
+
+
+# define a function that is responsible for "rolling" dice
+# this function will use random.randint() for the random generation of a die
+# (should we include the following instructions?: can either return the value of all or one at a time)
+
+def roll():
+
+
+# Both computer and player below:
+
+
+# Final score reported to user:
