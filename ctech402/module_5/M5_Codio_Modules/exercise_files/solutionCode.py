90557d65be0d9885b2e8e57ea331ebc6fcd2a0d2
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-10-25 16:02:02 -0400

    Add files via upload

---
+++ ctech402/module_5/M5_Codio_Modules/exercise_files/solutionCode.py
@@ -0,0 +1,33 @@
+# A module of statistical functions.
+
+# Calculate the mean of a list of numbers.
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+# Calculate the median of a list of numbers.
+def median(numbers):
+    # First, sort the numbers.
+    sorted_numbers = sorted(numbers)
+
+    # If we have an odd length list, pick the middle one.
+    if len(numbers) % 2 == 0:
+        return sorted_numbers[len(numbers) // 2]
+
+    # If we have an even length list, average the middle two.
+    else:
+        middle1 = sorted_numbers[len(numbers) // 2]
+        middle2 = sorted_numbers[len(numbers) // 2 - 1]
+        return (middle1 + middle2) / 2.0
+
+# Calculate the range (biggest minus smallest).
+def range(numbers):
+    return max(numbers) - min(numbers)
+
+# Calculate the sum of all numbers
+def sum(numbers):
+	
+	total_sum = 0
+	for i in numbers:
+		total_sum = total_sum + i
+		
+	return total_sum
