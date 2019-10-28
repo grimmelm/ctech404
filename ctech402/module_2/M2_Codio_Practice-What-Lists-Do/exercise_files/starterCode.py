1d5bb716afbff314db090328b97bb07bb777017f
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-10-28 09:34:57 -0400

    Create starterCode.py

---
+++ ctech402/module_2/M2_Codio_Practice-What-Lists-Do/exercise_files/starterCode.py
@@ -0,0 +1,3 @@
+int_list = [100, 5, 14, 1, 3, 7, 26, 37]
+str_list = ['Cat', 'Rabbit', 'Dog', 'Guinea Pig', 'Hamster', 'Gold Fish']
+mixed_list = [1.5, 'Monday', 75, 'Spoon', 25, 0, '4.5', 'Socks', '7']
