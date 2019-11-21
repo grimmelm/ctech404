bd88ceb37adecd428eb2e9e902fa0bd3de080cd4
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-11-21 14:28:09 -0500

    Update startercode3.py

--- ctech402/module_6/M6_Codio_PROJECT/exercise_files/startercode3.py
+++ ctech402/module_6/M6_Codio_PROJECT/exercise_files/startercode3.py
@@ -1,1 +1,26 @@
 
+####
+# Player and Computer each have 3 dice
+# They each roll all 3 dice until one player rolls 3 matching dice.
+#######
+
+# import random package
+import random
+
+# player and computers total score
+player_score = 0
+computer_score = 0
+
+
+# define a function that is checks for three matching dice
+# function returns True or False
+
+
+
+# Computer and user roll until one rolls 3 matching dice
+# Display the 3 dice with each roll
+
+
+
+
+# Print both the player and computer's final score and report who wins 
