2abf2fbbc6c804fae57eb351ba83b70d434179ae
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-11-27 12:37:43 -0500

    Update SolutionCode.py

--- ctech402/module_4/M4_Codio_Using-Dictionaries/exercise_files/SolutionCode.py
+++ ctech402/module_4/M4_Codio_Using-Dictionaries/exercise_files/SolutionCode.py
@@ -1,9 +1,20 @@
-weekly_planner = { 'Monday': 'hair appointment' , 'Tuesday': 'pick up groceries', 'Wednesday': 'baseball practice', 'Thursday': 'wash car', 'Friday': 'dinner with friends'}
+weekly_planner = { 'Monday': 'hair appointment' , 'Tuesday': 'pick up groceries', 'Wednesday': 'baseball practice', 'Thursday': 'wash car', 'Saturday': 'dinner with friends'}
 
+
+moved_appointments = []
 if 'Monday' in weekly_planner:
-  weekly_planner.pop('Monday')
+	moved_appointments.append(weekly_planner['Monday'])
+	del weekly_planner['Monday']
+
+if 'Friday' in weekly_planner:
+	moved_appointments.append(weekly_planner['Friday'])
+	del weekly_planner['Friday']
+	
+
+weekly_planner['Tuesday'] += ',' + ' ,'.join(moved_appointments)
   
-if 'Wednesday' in weekly_planner:
-  weekly_planner.pop('Wednesday')
   
 print(weekly_planner)
+
+
+#want monday and friday off, move to another day of the week
