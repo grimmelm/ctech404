0fba95be4f53412196193dadb0e2823642b4d7db
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-11-27 15:19:36 -0500

    Update StarterCode.py

--- ctech402/module_5/M5_Codio_Modules/StarterCode.py
+++ ctech402/module_5/M5_Codio_Modules/StarterCode.py
@@ -2,15 +2,14 @@ import stats
 
 my_list = [4,1,5,7,6,8,9,10,8,3,3,8,12]
 
+my_mean  = stats.mean(my_list)
+print('The mean is: ' +  str(my_mean))
 
-mean  = stats.mean(my_list)
-print('The mean is: ' +  str(mean))
+my_median = stats.median(my_list)
+print('The median is: ' +  str(my_median))
 
-median = stats.median(my_list)
-print('The median is: ' +  str(median))
+my_range = stats.range(my_list)
+print('The range is: ' +  str(my_range))
 
-range = stats.range(my_list)
-print('The range is: ' +  str(range))
-
-std_dev = stats.standard_deviation(my_list)
-print('The standard deviation is: ' + str(std_dev))
+my_std_dev = stats.standard_deviation(my_list)
+print('The standard deviation is: ' + str(my_std_dev))
