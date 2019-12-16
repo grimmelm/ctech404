febf9fbb56964e16716ec654f8efa94adfb5c4c6
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-12-16 11:49:13 -0500

    Update SolutionCode.py

--- ctech402/module_2/M2_Codio_PROJECT-Remove-Append/exercise_files/SolutionCode.py
+++ ctech402/module_2/M2_Codio_PROJECT-Remove-Append/exercise_files/SolutionCode.py
@@ -1,47 +1,13 @@
-prime_numbers = [ 2, 10, 25, 11, 5, 3, 97, 93, 100]
-remove = []
+integer_list = [10,2,5,7,4,3,6,9,8,1]
+list_even = []
+list_odd = []
 
-for i in prime_numbers:
-	for n in range(2, i): # 2,3,4,5...up to the array value
-		if i % n == 0:
-			remove.append(i)
-			#break
-
-for r in remove:
-	if r in prime_numbers:
-		prime_numbers.remove(r)
-------------------
-# Remove all odd numbers:
-	
-numbers = [ 2, 10, 25, 11, 5, 3, 97, 93, 100]
-items_to_remove = []
-
-for i in numbers:
-	if i % 2 == 1:
-		items_to_remove.append(i)
-			
-
-for r in items_to_remove:
-	numbers.remove(r)
-
-print(numbers)
-
------------------------
-
-input_list = [1,2,3,4,5,6,7,8,9,10]
-list_A = []
-list_B = []
-
-for i in len(input_list):
+for i in integer_list:
 	if i%2 == 0:
-		list_A.append(input_list[i]))
+		list_even.append(i)
 	else:
-		list_B.append(input_list[i]))
+		list_odd.append(i)
 
-merged_list = list_A.extend(list_B)
 
-for i in range(len(list_A)):
-	merged_list.append(list_A[i])
-	merged_list.append(list_B[i])
-	
-	
+print("Even numbers: " + str(sorted(list_even)))
+print("Odd numbers: " + str(sorted(list_odd)))
