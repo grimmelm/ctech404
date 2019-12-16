2380800ad9ba0a139f71207facad7609b98aabe7
gisnerEC <47575584+gisnerEC@users.noreply.github.com>
2019-12-16 11:54:19 -0500

    Update SolutionCode.py

--- ctech402/module_2/M2_Codio_PROJECT-Remove-Append/exercise_files/SolutionCode.py
+++ ctech402/module_2/M2_Codio_PROJECT-Remove-Append/exercise_files/SolutionCode.py
@@ -15,3 +15,16 @@ print("Even numbers: " + str(sorted(list_even)))
 print("Odd numbers: " + str(sorted(list_odd)))
 
 Part 2:
+
+integer_list = [10, 2, 5, 7, 4, 3, 6, 9, 8, 1]
+
+integer_list.sort()
+
+list_a = list(reversed(integer_list))
+
+integer_list.extend(list_a)
+
+index_of_max = integer_list.index(max(integer_list))
+integer_list.pop(index_of_max)
+
+print(integer_list)
