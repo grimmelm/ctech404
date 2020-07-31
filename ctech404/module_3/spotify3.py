59cf84e2fe859c45f90d1b87d769c0d26c5714c5
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 13:52:28 -0400

    Create spotify3.py

---
+++ ctech404/module_3/spotify3.py
@@ -0,0 +1,21 @@
+import requests, sys
+from spotifysecrets import *
+from base64 import b64encode
+import requestsview
+
+AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token'
+
+# Authenticate with Spotify
+headers = {'Authorization':
+           b'Basic ' + b64encode(CLIENT_ID + b':' + CLIENT_SECRET)}
+body = {'grant_type': 'client_credentials'}
+r = requests.post(AUTHENTICATION_URL, headers=headers, data=body)
+
+token = r.json()['access_token']
+token_headers = {'Authorization' : 'Bearer ' + token}
+
+SEARCH_URL = 'https://api.spotify.com/v1/search'
+
+# Find top artist matching the user's search
+artist_name = ' '.join(sys.argv[1:])
+print('Artist name: + 'artist_name_)
