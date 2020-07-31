9072afd0e7061b186bf46f58b30e8ccd92365aeb
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 14:19:25 -0400

    Update spotify7.py

--- ctech404/module_3/spotify7.py
+++ ctech404/module_3/spotify7.py
@@ -1,6 +1,7 @@
 import requests, sys
 from spotifysecrets import *
 from base64 import b64encode
+import requestview
 
 AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token'
 
@@ -11,20 +12,21 @@ body = {'grant_type': 'client_credentials'}
 r = requests.post(AUTHENTICATION_URL, headers=headers, data=body)
 
 token = r.json()['access_token']
-tokenHeaders = {'Authorization' : 'Bearer ' + token}
+token_headers = {'Authorization' : 'Bearer ' + token}
 
 SEARCH_URL = 'https://api.spotify.com/v1/search'
 TOP_TRACKS_URL = 'https://api.spotify.com/v1/artists/'
 
 # Find top artist matching the user's search
-artistName = ' '.join(sys.argv[1:])
-query = {'q': artistName, 'type': 'artist'}
-r = requests.get(SEARCH_URL, headers=tokenHeaders, params=query)
+artist_name = ' '.join(sys.argv[1:])
+query = {'q': artist_name, 'type': 'artist'}
+r = requests.get(SEARCH_URL, headers=token_headers, params=query)
 artist = r.json()['artists']['items'][0]
 
 # Find top 10 tracks by that artist
 url = TOP_TRACKS_URL + artist['id'] + '/top-tracks'
 query = {'country': 'US'}
-r = requests.get(url, headers=tokenHeaders, params=query)
+r = requests.get(url, headers=token_headers, params=query)
+
 for track in r.json()['tracks']:
     print(track['name'])
