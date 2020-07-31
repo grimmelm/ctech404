63a8c349732881144f2376d5c5543c5ea8233d66
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 15:32:38 -0400

    Update and rename ctech404/module_5/tooter1/tooterclient1.py to ctech404/module_5/chirper1/chirperclient1.py

--- ctech404/module_5/chirper1/chirperclient1.py
+++ ctech404/module_5/chirper1/chirperclient1.py
@@ -3,53 +3,48 @@ import requests, sys
 BASE_URL= 'http://localhost'
 PORT = ":5000"
 
-GET_ENDPOINT = BASE_URL + PORT + '/get-toots'
-POST_ENDPOINT = BASE_URL + PORT + '/post-toot'
+GET_ENDPOINT = BASE_URL + PORT + '/get-chirps'
+POST_ENDPOINT = BASE_URL + PORT + '/post-chirp'
 
 
 def login():
     u = input('Enter username: ')
     return u
  
-def top_toots():
+def top_chirps():
     r = requests.get(GET_ENDPOINT)
     if r.ok:
-        print('-----TOOTS-----')
-        for toot in reversed(r.json()):
-            print(toot['userid'] + ' -> ' + toot['text'])
+        print('-----chirpS-----')
+        for chirp in reversed(r.json()):
+            print(chirp['userid'] + ' -> ' + chirp['text'])
         print('---------------')
     else:
         print('Error: ' + r.text)
 
  
-def post_toot(userid):
-    t = input('Enter your toot: ')
+def post_chirp(userid):
+    t = input('Enter your chirp: ')
     r = requests.post(POST_ENDPOINT, data={'text': t, 'user' : userid}})
     if r.ok:
-        print('Toot posted')
+        print('chirp posted')
     else:
         print('Error: ' + r.text)
 
 
 
 
-print('Welcome to Tooter!')
+print('Welcome to chirper!')
 u = input('Enter username')
 while True:
     print('(0) quit')
-    print('(1) read toots')
-    print('(2) post toot')
+    print('(1) read chirps')
+    print('(2) post chirp')
     i = input('Enter command : ')
     if i == '0':
         break
     elif i == '1':
-        get_toots()
+        get_chirps()
     elif i == '2':
-        post_toot(u)        
+        post_chirp(u)        
     else:
         print('Illegal command. Please try again.')
-
-
-
-
-            
