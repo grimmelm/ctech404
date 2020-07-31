a68ef22c19c07a9292e3485de2562f3132b3c728
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 13:35:11 -0400

    Update spotify1.py

--- ctech404/module_3/spotify1.py
+++ ctech404/module_3/spotify1.py
@@ -1,6 +1,7 @@
 import requests, sys
 from spotifysecrets import *
 from base64 import b64encode
+import requestview
 
 AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token'
 
@@ -12,8 +13,6 @@ r = requests.post(AUTHENTICATION_URL, headers=headers, data=body)
 
 token = r.json()['access_token']
 
-from requests_toolbelt.utils import dump
-data = dump.dump_all(r, request_prefix = '', response_prefix='')
-print(data.decode('utf-8'))
+requestview.view(r)
 
-print('\nToken: ' + token)
+print('Token: ' + token)
