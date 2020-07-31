66785721c1bf268535c61ecbc7487b10f53d57d7
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 13:53:52 -0400

    Update spotify4.py

--- ctech404/module_3/spotify4.py
+++ ctech404/module_3/spotify4.py
@@ -1,7 +1,7 @@
 import requests, sys
 from spotifysecrets import *
 from base64 import b64encode
-import requestsview
+import requestview
 
 AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token'
 
@@ -12,13 +12,13 @@ body = {'grant_type': 'client_credentials'}
 r = requests.post(AUTHENTICATION_URL, headers=headers, data=body)
 
 token = r.json()['access_token']
-tokenHeaders = {'Authorization' : 'Bearer ' + token}
+token_headers = {'Authorization' : 'Bearer ' + token}
 
 SEARCH_URL = 'https://api.spotify.com/v1/search'
 
 # Find top artist matching the user's search
-artistName = ' '.join(sys.argv[1:])
-query = {'q': artistName, 'type': 'artist'}
-r = requests.get(SEARCH_URL, headers=tokenHeaders, params=query)
+artist_name = ' '.join(sys.argv[1:])
+query = {'q': artist_name, 'type': 'artist'}
+r = requests.get(SEARCH_URL, headers=token_headers, params=query)
 
-requestsview.view(r)
+requestview.view(r)
