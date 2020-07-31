4a5840740f5d718f91606390749fab2bb7e89d91
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 15:33:33 -0400

    Update and rename ctech404/module_5/tooter1/tooterserver1.py to ctech404/module_5/chirper1/chirpererver1.py

---
+++ ctech404/module_5/chirper1/chirpererver1.py
@@ -0,0 +1,28 @@
+import json, time
+import flask
+import basicdb
+
+DATABASE = 'chirper.json'
+
+# Setup
+app = Flask(__name__)
+basicdb.open_db(DATABASE)
+   
+@app.route('/get-chirps')
+def get_chirps():
+    chirps = basicdb.orderby(basicdb.sql_from('chirps'), 'time')))[-5:]
+    return json.dumps(latest_chirps)
+
+@app.route('/post-chirp', methods=['POST'])
+def post_chirp():
+    chirp = {'userid': flask.request.form['user'],
+            'time': str(time.time()), 
+            'text': flask.request.form['text']}
+    if len(chirp['text'] > 100):
+        return 'chirp too long', 403
+    basicdb.insert('chirps', chirp)
+    return 'chirp posted'
+
+
+
+app.run(host='0.0.0.0')
