ec814f3cc78c8c78a1538dde4cf42c3556494979
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 13:47:46 -0400

    Create spotify2.py

---
+++ ctech404/module_3/spotify2.py
@@ -0,0 +1,16 @@
+import requests, sys
+from spotifysecrets import *
+from base64 import b64encode
+import requestview
+
+AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token'
+
+# Authenticate with Spotify
+headers = {'Authorization':
+           b'Basic ' + b64encode(CLIENT_ID + b':' + CLIENT_SECRET)}
+body = {'grant_type': 'client_credentials'}
+r = requests.post(AUTHENTICATION_URL, headers=headers, data=body)
+
+token = r.json()['access_token']
+
+print(token)
