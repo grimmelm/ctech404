4ae8b97f5c1e1bd1f71dc61e713c358de9ba862c
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-07-31 11:43:14 -0400

    Update spotifysecrets.py

--- ctech404/module_3/spotifysecrets.py
+++ ctech404/module_3/spotifysecrets.py
@@ -1,4 +1,2 @@
-# This is my old client secret. It won't work now. :-)
-# You'll need your own to make this code work.
 CLIENT_ID = b'543493c94bd14a718fdb2691a5614338'
 CLIENT_SECRET = b'b02a1f58313c47feafe2cd4d23a7f587'
