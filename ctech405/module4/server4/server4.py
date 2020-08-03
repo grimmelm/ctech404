bca32d3d284b1d689007006da9571db6a12e5c59
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-08-03 17:16:58 -0400

    Create server4.py

---
+++ ctech405/module4/server4/server4.py
@@ -0,0 +1,8 @@
+import flask
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return flask.render_template('hello.html', secretword=request.args['password'])
+
+app.run(host='0.0.0.0')
