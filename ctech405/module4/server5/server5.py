922371f5d0f8b8da1a39f5e4276dad5c6dc63cb8
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-08-03 17:21:01 -0400

    Update server5.py

--- ctech405/module4/server5/server5.py
+++ ctech405/module4/server5/server5.py
@@ -4,6 +4,6 @@ app = flask.Flask(__name__)
 @app.route('/')
 def hello_world():
     words = ['swordfish', 'xyzzy', 'Tyler sent me']
-    return render_template('hello.html', secretwords=words)
+    return flask.render_template('secret.html', secretwords=words)
 
 app.run(host='0.0.0.0')
