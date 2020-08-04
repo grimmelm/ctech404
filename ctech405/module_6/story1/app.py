7f6a8ed85d68179e123074b3a8baf82cf7cb40f4
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-08-04 11:02:53 -0400

    Update app.py

--- ctech405/module_6/story1/app.py
+++ ctech405/module_6/story1/app.py
@@ -4,12 +4,12 @@ app = flask.Flask(__name__)
 story = []
 
 @app.route('/')
-def form_show():
+def story_start():
     global story = []
     return flask.render_template('start.html')
 
 @app.route('/add')
-def form_show():
+def story_continue():
     m = request.form['new-text']
     story.append(m)
     return flask.render_template('add.html', words = story)
