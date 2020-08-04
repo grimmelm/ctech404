03001c05fbbaf6664670d1f1392760e74e594908
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-08-04 09:39:36 -0400

    Update form.py

--- ctech405/module_5/form3/form.py
+++ ctech405/module_5/form3/form.py
@@ -5,8 +5,12 @@ app = flask.Flask(__name__)
 def form_show():
     return flask.render_template('form.html')
 
-@app.route('/done')
+@app.route('/done1')
 def form_submit():
-    return flask.render_template('result.html', fields = flask.request.args, url=flask.request.url)
+    return flask.render_template('result.html', fields = flask.request.args, url=flask.request.url, form = 'Form 1')
+
+@app.route('/done2')
+def form_submit():
+    return flask.render_template('result.html', fields = flask.request.args, url=flask.request.url, form = 'Form 2')
 
 app.run(host='0.0.0.0')
