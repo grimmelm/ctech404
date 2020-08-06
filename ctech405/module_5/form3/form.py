949f0a29ad8d6dfb7f23a3ac531c2a1fb6edfc47
James Grimmelmann <james.grimmelmann@cornell.edu>
2020-08-06 16:14:26 -0400

    CTECH 405 edits to module 5

--- ctech405/module_5/form3/form.py
+++ ctech405/module_5/form3/form.py
@@ -6,11 +6,11 @@ def form_show():
     return flask.render_template('form.html')
 
 @app.route('/done1')
-def form_submit():
+def form_two_submit():
     return flask.render_template('result.html', fields = flask.request.args, url=flask.request.url, form = 'Form 1')
 
 @app.route('/done2')
-def form_submit():
+def form_one_submit():
     return flask.render_template('result.html', fields = flask.request.args, url=flask.request.url, form = 'Form 2')
 
 app.run(host='0.0.0.0')
